106c25fcb6eb3f12e858c77f73aef3da13b16892
mdubravski <mdubravski@gmail.com>
2021-10-21 18:22:26 -0700

    Uploaded Examples

---
+++ buttons/index.js
@@ -0,0 +1,11 @@
+function disable(){
+    document.getElementById("btn1").disabled = true;
+}
+
+function undisable(){
+    document.getElementById("btn1").disabled = false;
+}
+
+function doSomething(){
+    document.getElementById("test").innerHTML = "Goodbye";
+}
